6ba9eb71f145601270bfd95f335cc1367b4e1904
Konstantin <kshevworks@gmail.com>
2025-10-01 18:03:09 +0300

    added main screen

---
+++ Assets/SandboxUI/Scripts/ButtonView.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SandboxUI.Scripts
+{
+    public class ButtonView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        [SerializeField] private UnityEvent _onClick;
+
+        [Header("Styles/General")]
+        [SerializeField] private Graphic[] _elements;
+        [SerializeField] private Graphic _bg;
+        [SerializeField] private GameObject _arrow;
+
+        [Header("Style/Normal")]
+        [SerializeField] private Color _elementNormalColor;
+        [SerializeField] private Color _bgNormalColor;
+
+        [Header("Style/Hover")]
+        [SerializeField] private Color _elementHoverColor;
+        [SerializeField] private Color _bgHoverColor;
+
+        private void Awake()
+        {
+            _arrow.SetActive(false);
+            SetNormalState();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            SetHoverState();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("Click");
+            _onClick.Invoke();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            SetNormalState();
+        }
+
+        [ContextMenu("Button State/Normal")]
+        private void SetNormalState()
+        {
+            foreach (var graphic in _elements)
+            {
+                graphic.color = _elementNormalColor;
+            }
+
+            _bg.color = _bgNormalColor;
+
+            _arrow.SetActive(false);
+        }
+
+        [ContextMenu("Button State/Hover")]
+        private void SetHoverState()
+        {
+            foreach (var graphic in _elements)
+            {
+                graphic.color = _elementHoverColor;
+            }
+
+            _bg.color = _bgHoverColor;
+
+            _arrow.SetActive(true);
+        }
+    }
+}
