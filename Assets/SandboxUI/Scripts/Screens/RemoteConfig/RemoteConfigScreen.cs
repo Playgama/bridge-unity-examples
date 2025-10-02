54897a87d4e7f4aa059dba5a5e3424cad937e124
Konstantin <kshevworks@gmail.com>
2025-10-02 22:46:44 +0300

    implemented remote config screen logic

---
+++ Assets/SandboxUI/Scripts/Screens/RemoteConfig/RemoteConfigScreen.cs
@@ -0,0 +1,16 @@
+﻿using Playgama;
+using SandboxUI.Scripts.Base;
+using UnityEngine;
+
+namespace SandboxUI.Scripts.Screens.RemoteConfig
+{
+    public class RemoteConfigScreen : MonoBehaviour
+    {
+        [SerializeField] private PropertyTextView _isRemoteConfigSupported;
+
+        private void OnEnable()
+        {
+            _isRemoteConfigSupported.SetText(Bridge.remoteConfig.isSupported.ToString());
+        }
+    }
+}
---
+++ Assets/SandboxUI/Scripts/Screens/RemoteConfig/Requests/GetRemoteConfigBridgeRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Playgama;
+using SandboxUI.Scripts.Base;
+
+namespace SandboxUI.Scripts.Screens.RemoteConfig.Requests
+{
+    public class GetRemoteConfigBridgeRequestHandler : BaseBridgeRequestWithMultilineResponseHandler
+    {
+        public override void SendRequest()
+        {
+            Bridge.remoteConfig.Get(HandleRequestCompleted);
+        }
+
+        private void HandleRequestCompleted(bool success, Dictionary<string, string> response)
+        {
+            if (!success)
+            {
+                SetResponse("Request failed.");
+                return;
+            }
+
+            var strResponse = JsonConvert.SerializeObject(response);
+            SetResponse(strResponse);
+        }
+    }
+}
