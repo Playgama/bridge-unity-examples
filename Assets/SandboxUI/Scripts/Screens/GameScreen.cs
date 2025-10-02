f33228ef2fa1e601530f061376c4155e6d69ea1d
Konstantin <kshevworks@gmail.com>
2025-10-02 19:00:46 +0300

    implemented game screen logic

---
+++ Assets/SandboxUI/Scripts/Screens/GameScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Playgama;
+using UnityEngine;
+
+namespace SandboxUI.Scripts.Screens
+{
+    public class GameScreen : MonoBehaviour
+    {
+        [SerializeField] private PropertyTextView _currentVisibilityState;
+        [SerializeField] private PropertyTextView _lastVisibilityState;
+
+        private void OnEnable()
+        {
+            _currentVisibilityState.SetText(Bridge.game.visibilityState.ToString());
+            _lastVisibilityState.SetText("true visible ->");
+        }
+    }
+}
