930c88ea640f6153e67dacec591f7c653f58d768
Konstantin <kshevworks@gmail.com>
2025-10-02 19:31:26 +0300

    fixed initial toggle behavior at storage screen

--- Assets/SandboxUI/Scripts/Screens/StorageScreen.cs
+++ Assets/SandboxUI/Scripts/Screens/StorageScreen.cs
@@ -1,6 +1,8 @@
-﻿using Playgama;
+﻿using System;
+using Playgama;
 using Playgama.Modules.Storage;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SandboxUI.Scripts.Screens
 {
@@ -11,11 +13,26 @@ namespace SandboxUI.Scripts.Screens
         [SerializeField] private PropertyTextView _isLocalStorageAvailable;
         [SerializeField] private PropertyTextView _isPlatformInternalSupported;
         [SerializeField] private PropertyTextView _isPlatformInternalAvailable;
+        [SerializeField] private Toggle _localStorageToggle;
+        [SerializeField] private Toggle _platformInternalToggle;
 
 
         private void OnEnable()
         {
-            _defaultStorageType.SetText(Bridge.storage.defaultType.ToString());
+            var storageDefaultType = Bridge.storage.defaultType;
+            switch (storageDefaultType)
+            {
+                case StorageType.LocalStorage:
+                    _localStorageToggle.isOn = true;
+                    break;
+                case StorageType.PlatformInternal:
+                    _platformInternalToggle.isOn = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            _defaultStorageType.SetText(storageDefaultType.ToString());
             _isLocalStorageSupported.SetText(Bridge.storage.IsSupported(StorageType.LocalStorage).ToString());
             _isLocalStorageAvailable.SetText(Bridge.storage.IsAvailable(StorageType.LocalStorage).ToString());
             _isPlatformInternalSupported.SetText(Bridge.storage.IsSupported(StorageType.PlatformInternal).ToString());
