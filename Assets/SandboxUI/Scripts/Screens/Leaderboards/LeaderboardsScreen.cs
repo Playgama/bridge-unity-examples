fa81b0c7ba0102e56776e655fddaed8d732e1c0a
Konstantin <kshevworks@gmail.com>
2025-10-02 22:20:04 +0300

    implemented leaderboards screen logic

---
+++ Assets/SandboxUI/Scripts/Screens/Leaderboards/LeaderboardsScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Playgama;
+using SandboxUI.Scripts.Base;
+using UnityEngine;
+
+namespace SandboxUI.Scripts.Screens.Leaderboards
+{
+    public class LeaderboardsScreen : MonoBehaviour
+    {
+        [SerializeField] private PropertyTextView _leaderboardsType;
+
+        private void OnEnable()
+        {
+            _leaderboardsType.SetText(Bridge.leaderboards.type.ToString());
+        }
+    }
+}
---
+++ Assets/SandboxUI/Scripts/Screens/Leaderboards/Requests/SetScoreBridgeRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using Playgama;
+using SandboxUI.Scripts.Base;
+using TMPro;
+using UnityEngine;
+
+namespace SandboxUI.Scripts.Screens.Leaderboards.Requests
+{
+    public class SetScoreBridgeRequestHandler : BaseBridgeRequestHandler
+    {
+        [SerializeField] private TMP_InputField _scoreInput;
+        [SerializeField] private TMP_InputField _leaderboardIdInput;
+
+        public override void SendRequest()
+        {
+            if (string.IsNullOrEmpty(_scoreInput.text) || string.IsNullOrEmpty(_leaderboardIdInput.text))
+            {
+                return;
+            }
+
+            if (!int.TryParse(_scoreInput.text, out var score))
+            {
+                return;
+            }
+
+            Bridge.leaderboards.SetScore(_leaderboardIdInput.text, score);
+        }
+    }
+}
