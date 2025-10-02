859fe74b6dc490d4214df4cccd3c3a8823e2ec93
Konstantin <kshevworks@gmail.com>
2025-10-02 16:01:37 +0300

    recomposition

---
+++ Assets/SandboxUI/Scripts/PropertyTextView.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SandboxUI.Scripts
+{
+    public class PropertyTextView : MonoBehaviour
+    {
+        private const char Delimiter = ':';
+
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private string _name;
+        [SerializeField] private Color _valueColor;
+
+        public void SetText(string value)
+        {
+            _text.text = $"{_name}{Delimiter} <color=#{ColorUtility.ToHtmlStringRGB(_valueColor)}{value}</color>";
+        }
+    }
+}
